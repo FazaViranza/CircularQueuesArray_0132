08f5110c8eab154557e19f80522a576703ce89d3
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-06-12 15:21:11 +0700

    case 4

--- Queues.cpp
+++ Queues.cpp
@@ -153,8 +153,11 @@ int main()
                 q.display();
                 break;
             }
-       
-        
+            case '4':
+            {
+                return 0;
+            }
+         
         
     }
 }
